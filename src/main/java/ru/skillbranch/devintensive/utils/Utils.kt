0f785f11d33af4f8647ca9053cfb60906838e81d
MIkFogel <life904@yandex.ru>
2020-02-23 18:43:46 +0300

    parser

--- src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,3 +4,16 @@ enum class TypeMessage {
     IMAGE, TEXT
 }
 
+object Utils {
+    fun parseFullName(fullName: String?) : Pair<String?, String?> {
+
+        var fullName = if (fullName == "" || fullName == " ") null else fullName
+
+        val splitted : List<String>? = fullName?.split(" ")
+
+        val firstName = splitted?.getOrNull(0)
+        val lastName = splitted?.getOrNull(1)
+
+        return Pair(firstName, lastName)
+    }
+}
